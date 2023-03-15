51e16ce423b2f8e005a147e0453aeaad3322f48b
Claudio Salas <djsalas2133@gmail.com>
2023-03-15 13:32:10 -0700

    pic picker and crop installed

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
-
-const PlaceholderImage = require('./assets/images/leia-bear.png');
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
+
+
+
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <Text style={styles.imageText}> Hi, I'm Leia Bear </Text>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,11 +46,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'aquamarine',
+    backgroundColor: '#25292e',
     alignItems: 'center',
   },
   imageContainer: {
-    flex: 1,
+    flex:1,
     paddingTop: 58,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageText: {
+    color: 'aquamarine',
+    fontSize: 20,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  footerText: {
+    color: 'white',
+    fontSize: 16,
+    alignItems: 'center',
+
   },
 });
